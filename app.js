9de8d1d11ed6bab24bafe9adddeca7f5f446c265
philliprognerud <philliprognerud2@yahoo.com>
2017-07-06 03:55:06 +0000

    added more files tot eh porject

--- app.js
+++ app.js
@@ -7,6 +7,16 @@ app.get("/", function(req, res){
     res.render("landing")
 });
 
+app.get("/campgrounds", function(req, res){
+    var campgrounds = [
+            {name: "Salmon Creek", image: "http://camprrm.com/wp-content/uploads/2011/06/whiteface1.jpg"},
+            {name: "Mtn Goat", image: "http://img1.sunset.timeinc.net/sites/default/files/styles/1000x1000/public/image/2016/10/main/hoodview-campground-0510.jpg?itok=B8Eb65Uf"},
+            {name: "Montara", image: "http://www.northshorevisitor.com/wp-content/uploads/2015/05/grand-marais-campground-1.jpg"},
+        ];
+        
+        res.render("campgrounds", {campgrounds:campgrounds});
+});
+
 
 app.listen(process.env.PORT, process.env.IP, function(){
    console.log("server has started"); 
