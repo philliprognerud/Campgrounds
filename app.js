3d9017bd586659ef986af150ef75d39b60ed08b1
philliprognerud <philliprognerud2@yahoo.com>
2017-07-11 20:34:14 +0000

    started using mongoose

--- app.js
+++ app.js
@@ -2,31 +2,59 @@ var express = require("express");
 var app = express();
 
 var bodyParser = require("body-parser");
+var mongoose = require("mongoose");
 
+mongoose.Promise = global.Promise;
+mongoose.connect("mongodb://localhost/campgrounds");
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 
-var campgrounds = [
-            {name: "Salmon Creek", image: "http://camprrm.com/wp-content/uploads/2011/06/whiteface1.jpg"},
-            {name: "Mtn Goat", image: "http://img1.sunset.timeinc.net/sites/default/files/styles/1000x1000/public/image/2016/10/main/hoodview-campground-0510.jpg?itok=B8Eb65Uf"},
-            {name: "Montara", image: "http://www.northshorevisitor.com/wp-content/uploads/2015/05/grand-marais-campground-1.jpg"},
-        ];
+var campgroundSchema = new mongoose.Schema({
+   name: String,
+   image: String
+});
+
+var Campground = mongoose.model("Campground", campgroundSchema);
+
+// Campground.create(
+//     {
+//         name: "Montara", image: "http://www.northshorevisitor.com/wp-content/uploads/2015/05/grand-marais-campground-1.jpg"
+        
+//     }, function(err, campground){
+//         if(err){
+//             console.log("error");
+//         } else {
+//             console.log(campground);
+//         }
+//     });
+
 
 app.get("/", function(req, res){
     res.render("landing")
 });
 
 app.get("/campgrounds", function(req, res){
-        res.render("campgrounds", {campgrounds:campgrounds});
+    Campground.find({}, function(err, allCampgrounds){
+        if(err){
+            console.log("error");
+        } else {
+            res.render("campgrounds", {campgrounds:allCampgrounds});
+        }
+    });
 });
 
 app.post("/campgrounds", function(req, res){
     var name = req.body.name;
     var image = req.body.image;
     var newCampground = {name: name, image: image}
-    campgrounds.push(newCampground);
     
-    res.redirect("/campgrounds");
+    Campground.create(newCampground, function(err, newlyCreated){
+       if(err){
+           console.log("error");
+       } else {
+           res.redirect("/campgrounds");
+       }
+    });
 });
 
 app.get("/campgrounds/new", function(req, res){
