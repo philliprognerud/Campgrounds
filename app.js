fc8bf9c4790a51db87e727af19161792c219e374
philliprognerud <philliprognerud2@yahoo.com>
2017-07-20 09:54:27 +0000

    added authentication feature

--- app.js
+++ app.js
@@ -17,6 +17,20 @@ app.set("view engine", "ejs");
 app.use(express.static(__dirname + "/public"));
 seedDB();
 
+//PASSPORT CONFIG
+app.use(require("express-session")({
+    secret: "this is the secret",
+    resave: false,
+    saveUninitialized: false
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(new LocalStrategy(User.authenticate()));
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
+
 
 //Home page
 app.get("/", function(req, res){
@@ -100,6 +114,27 @@ app.post("/campgrounds/:id/comments", function(req, res){
                 }
             });
         }
+    });
+});
+
+
+//////////////////////////////
+////// AUTH ROUTES
+//////////////////////////////
+app.get("/register", function(req, res){
+    res.render("register");
+});
+
+app.post("/register", function(req,res){
+    var newUser = new User({username: req.body.username});
+    User.register(newUser, req.body.password, function(err, user){
+       if(err){
+           console.log(err);
+           return res.render("register");
+       } 
+       passport.authenticate("local")(req, res, function(){
+          res.redirect("/campgrounds"); 
+       });
     });
 });
 
