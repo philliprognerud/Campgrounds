36a5b840bdc3aa1ea7e3e9c34d0df00b57f1150a
philliprognerud <philliprognerud2@yahoo.com>
2017-07-25 03:47:20 +0000

    finished adding the flash messages

--- routes/index.js
+++ routes/index.js
@@ -18,10 +18,10 @@ router.post("/register", function(req,res){
     var newUser = new User({username: req.body.username});
     User.register(newUser, req.body.password, function(err, user){
        if(err){
-           console.log(err);
-           return res.render("register");
+           return res.render("register", {"error": err.message});
        } 
        passport.authenticate("local")(req, res, function(){
+           req.flash("success", "Welcome to the campgrounds " + user.username);
           res.redirect("/campgrounds"); 
        });
     });
@@ -29,7 +29,7 @@ router.post("/register", function(req,res){
 
 //show login form
 router.get("/login", function(req,res){
-   res.render("login", {message: req.flash("error")}); 
+   res.render("login"); 
 });
 
 //handling login logic
@@ -42,16 +42,8 @@ router.post("/login", passport.authenticate("local", {
 //LOGOUT ROUTE
 router.get("/logout", function(req, res){
   req.logout();
+  req.flash("success", "Logged you out!");
   res.redirect("/campgrounds");
 });
-
-//middleware
-function isLoggedIn(req, res, next){
-    if(req.isAuthenticated()){
-        return next();
-    }
-    
-    res.redirect("/login");
-}
 
 module.exports = router;
